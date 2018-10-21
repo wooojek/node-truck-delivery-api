7325bb43e99f4b68aac2c1a34eb6856716018bd4
wooojek <yokomura123@gmail.com>
2018-10-21 22:50:00 +0200

    Finished endpoint

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 const Koa = require('koa');
 const mount = require('koa-mount');
 const serve = require('koa-static');
+const body = require('koa-body');
 const delivery = require('./routes/delivery');
 
 const app = new Koa();
 const static = new Koa();
+app.use(body());
 
 static.use(serve(__dirname + '/public'));
 app.use(mount('/', static));
--- handlers/delivery.js
+++ handlers/delivery.js
@@ -1,3 +1,25 @@
+const countMinTrucks = require('../lib/countMinTrucks');
+const uuid = require('uuid/v4');
+
 module.exports = async (ctx) => {
-    ctx.body = 'Hello';
+    const packageData = ctx.request.body.data;
+    let trucks = countMinTrucks(packageData, 1000);
+    let price = 0;
+
+    trucks = trucks.map((load) => {
+        price += load.reduce((acc, package) => {
+            let singlePrice = package.weight > 400 ? (2 + 0.005 * package.weight) : (0.01 * package.weight);
+            return acc + singlePrice;
+        }, 0);
+
+        return {
+            load,
+            truckID: uuid(),
+        }
+    });
+
+    ctx.body = {
+        price,
+        trucks
+    };
 }
